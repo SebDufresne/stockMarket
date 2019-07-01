8a362c725201bed9d031d4d21e7bfbdcb4f1ad94
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-01 02:11:59 +0000

    Fixed silent errors by assigning 0 to value declaration added a case for arrays of 2 elements.

--- stockMarket.js
+++ stockMarket.js
@@ -1,3 +1,4 @@
+'use strict';
 const getLarger = (a,b) => a > b ? a : b;
 
 const minOfArr = (arr) => arr.reduce((acc,x) => acc <= x ? acc : x, arr[0]);
@@ -5,7 +6,7 @@ const minOfArr = (arr) => arr.reduce((acc,x) => acc <= x ? acc : x, arr[0]);
 const maxOfArr = (arr) => arr.reduce((acc,x) => acc <= x ? x : acc, arr[0]);
 
 const maxDiff = (arr, begin, end) => {
-  let leftPart, rightPart, leftMax, rightMax;
+  let leftPart = 0, rightPart = 0, leftMax = 0, rightMax = 0;
 
   const mid = Math.floor(begin + end / 2);
 
@@ -19,7 +20,6 @@ const maxDiff = (arr, begin, end) => {
     } else {
       leftPart = maxDiff(leftSlice, 0, leftSlice.length - 1);
     }
-
     if (rightSlice.length === 1) {
       rightSlice.push(rightSlice[0]);
       rightPart = 0;
@@ -35,9 +35,17 @@ const maxDiff = (arr, begin, end) => {
 };
 
 const stockMarket = (arr) => {
-  const diff = maxDiff(arr, 0, arr.length - 1);
-  return diff > 0 ? diff : -1;
+  let delta;
+  switch (arr.length) {
+  case 2:
+    delta = arr[0] < arr[1] ? arr[1] - arr[0] : -1;
+    break;
+  default:
+    delta = maxDiff(arr, 0, arr.length - 1);
+  }
+  return delta > 0 ? delta : -1;
 };
 
 console.log(stockMarket([45, 24, 35, 31, 40, 38, 11]));
 console.log(stockMarket([7, 6, 5, 4, 3, 2, 1]));
+console.log(stockMarket([6, 7, 8]));
